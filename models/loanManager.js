d9bed6b1c9da2aecd7d3917b70c37218fca03cf0
Alexander Ivanov <alexanderivanov2277@gmail.com>
2023-03-17 13:47:37 +0200

    add functionality for search for loan applications/loans, accept offer, remove application

--- models/loanManager.js
+++ models/loanManager.js
@@ -1,7 +1,8 @@
 class LoanManager{
-    constructor(userManager, {loans, lenders}) {
+    constructor(userManager, {loanApplications, loans, lenders}) {
         this.userManager = userManager;
         this.idGenerator = new IdGenerator(localStorage.getItem("freeId"));
+        this.loanApplications = loanApplications;
         this.loans = loans;
         this.lenders = lenders;
     }
@@ -9,57 +10,99 @@ class LoanManager{
     handleLoanRequest ({borrowerIncome, requestedAmount, requestedTerm}) {
         this.userManager.setMonthlyIncomeForUser(borrowerIncome);
 
-        const requesterStats = this.getRequesterStats(borrowerIncome, requestedAmount, requestedTerm)
+        const requesterStats = this.getApplicationStats(borrowerIncome, requestedAmount, requestedTerm)
         const id = this.idGenerator.getId();
 
         const monthlyPayment = requestedAmount / requestedTerm;
-        const requestLoan = new Loan(id, requestedAmount, requestedTerm, monthlyPayment);
+
+        const loanApplication = new LoanApplication(id, requestedAmount, requestedTerm, monthlyPayment);
+        this.loanApplications.push(loanApplication);
         
-        this.loans.push(requestLoan);
-        saveInLocalStorage({"loans": this.loans});
+        saveInLocalStorage({"loanApplications": this.loanApplications});
+        
         this.userManager.setLoanId(id);
 
-        //  CAN BE SEPERATED
+        //  CAN BE SEPERATED TO RETURN IN LOAN CONTROLLER AND CONNECT WITH OVERVIEW CONTROLLER
         const pendingLoan = new Promise((resolve, reject) => {
             if (requesterStats.isEligibilit) {
-                setTimeout(() => resolve([requesterStats, requestLoan]), 10000);
+                setTimeout(() => resolve([requesterStats, loanApplication]), 10000);
             } else {
                 setTimeout(() => reject(id), 10000);
             }
         });
 
-        pendingLoan      
+        return pendingLoan      
         .then(([requesterStats, requestLoan]) => {
-            this.approveLoan(requesterStats, requestLoan.id);
+            this.approveLoanApplication(requesterStats, requestLoan.id);
+            return true;
         })
         .catch(err => {
-            this.rejected(err);
-        })
+            this.rejectedLoanApplication(err);
+            return false;
+        });
     }
 
-    approveLoan(requesterStats, id) {
-        console.log(id);
-        const loan = this.getLoan(id);
-        loan.status = "approved";
-        loan.offers = this.getOffers(requesterStats, loan);
-        saveInLocalStorage({"loans": this.loans});
+    approveLoanApplication(requesterStats, id) {
+        const loan = this.getLoanApplication(id);
+
+        if (loan) {
+            loan.status = "approved";
+            loan.offers = this.getOffers(requesterStats.interestRate, loan.requestedAmount, loan.requestedTerm, loan.monthlyPayment);
+            saveInLocalStorage({"loanApplications": this.loanApplications});
+        }
     }
 
-    rejected(id) {
-        const loan = this.getLoan(id);
-        loan.status = "rejected";
-        saveInLocalStorage({"loans": this.loans});
+    rejectedLoanApplication(id) {
+        const loan = this.getLoanApplication(id);
+
+        if (loan) {
+            loan.status = "rejected";
+            saveInLocalStorage({"loanApplications": this.loanApplications});
+        }
+    }
+
+    getLoanApplication(id) {
+        return this.loanApplications.find(loan => loan.id === id);
+    }
+    
+    getLoanApplications(ids=this.userManager.logged.loanIDs) {
+        return this.loanApplications.filter(loan => ids.includes(loan.id));
+    }
+
+    removeLoanApplication(id) {
+        const loanApplication= this.getLoanApplication(id);
+
+        loanApplication.status = "cancalled";
+        saveInLocalStorage({"loanApplications": this.loanApplications});
     }
 
     getLoan(id) {
-        return this.loans.find(loan => loan.id === id);
+        return this.loan.find(loan => loan.id === id);
     }
 
-    getLoans(ids=this.userManager.loanIDs) {
+    getLoans(ids=this.userManager.logged.loanIDs) {
+        console.log(ids);
         return this.loans.filter(loan => ids.includes(loan.id));
     }
 
-    getOffers({interestRate, requestedAmount, requestedTerm, monthlyPayment}) {
+    acceptOffer(loanApplication, offer) {
+        loanApplication.status = "accepted";
+        const {id, requestedAmount, requestedTerm, monthlyPayment} = loanApplication;
+        const {interestRate, lenderName} = offer;
+        // MAKE RIGHT CALCULATIONS for totalOwned Money
+        const totalOwnedAmount =  requestedAmount + (requestedAmount * (interestRate/100));
+        
+        const loan = new Loan(id, requestedAmount, requestedTerm, monthlyPayment, totalOwnedAmount, interestRate, lenderName);
+        this.loans.push(loan);
+
+        saveInLocalStorage({
+            "loans": this.loans,
+            "loanApplications": this.loanApplications,
+        })
+        // save in local storage
+    }
+
+    getOffers(interestRate, requestedAmount, requestedTerm, monthlyPayment) {
         const offers = [];
     
         this.lenders.forEach(lender => {
@@ -67,15 +110,17 @@ class LoanManager{
             // TODO NUMBER OF APPLICATIONS CREATE
             if (isOffer) {
                 const loanAmount = requestedAmount > lender.maxLoanAmount ? lender.maxLoanAmount : requestedAmount;
-                offers.push(new Offer(interestRate, loanAmount, monthlyPayment, requestedTerm));
+                console.log(loanAmount, monthlyPayment, requestedTerm);
+                const offer = new Offer(interestRate, loanAmount, monthlyPayment, requestedTerm, lender.name);
+                console.log(offer)
+                offers.push(offer);
             }
         })
 
         return offers
     }
 
-
-    getRequesterStats = (borrowerIncome, requestedAmount, requestedTerm) => {
+    getApplicationStats = (borrowerIncome, requestedAmount, requestedTerm) => {
         const yearIncome = borrowerIncome * 12;
         const interestRate = this.calculateInterestRate(yearIncome);
         const isEligibilit = this.calculateEligibility(yearIncome, requestedAmount, requestedTerm);
